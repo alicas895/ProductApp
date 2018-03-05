efc90d2223ceea27941ba6f2cabb15efc4148660
alicas895 <37063216+alicas895@users.noreply.github.com>
2018-03-05 02:22:21 -0500

    Update ProductsController.cs

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -13,7 +13,8 @@ namespace ProductsApp.Controllers
         { 
             new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
             new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M } 
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M },
+            new Product { Id = 4, Name = "Milk", Category = "Groceries", Price = 3 }
         };
 
         public IEnumerable<Product> GetAllProducts()
